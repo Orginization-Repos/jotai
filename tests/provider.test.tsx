3da5d0b2cfb85d258f48f5f1af3441679779896c
Logan Thies <logan.thies@icloud.com>
2021-02-16 21:57:36 -0800

    test/planning

--- tests/provider.test.tsx
+++ tests/provider.test.tsx
@@ -70,10 +70,43 @@ xit('only uses initial value from provider for specific atom', async () => {
   }
 
   const { getByText } = render(
-    <Provider initialValues={[[petAtom, 'dog']]}>
-      <Display />
+    <Provider initialValues={[[petAtom, 'dog']]}> //can we get state from this providor component
+      <useAtomic> //can we listen for useAtom() calls ==> if we can, can we get info? //what info does the fiber tree store// custom hook for 
+        <App> //test = atom(0)
+          <Child /> // useAtom(test) //atom1 //'Child' atom1:child //
+        </App>
+      </useAtomic>
     </Provider>
   )
+  atom1: { value: 4, dependents: [atom2,atom3] },
+  const x = {a:a} : refID = 325erd53g => [x, y, z]
+  const y = x
+  const z = x
+
+
+  const name = atom('logan');
+
+  const useName = useAtom(name)
+
+  {
+    Node: [x,]
+    State: [ljfalkjsflak]
+    parents: [a]
+    Children: [{Node: a, state, parents, childrem}, {Node: a, state, parents, childrem}, {Node: a, state, parents, childrem}]
+  }
+
+
+  
+
+  function useAtom(atom) {
+    console.log({atom}) ==> {atom: 4627d728} ==> [atom, useName]
+
+    return a + 2
+  }
+
+  test(x) ==> test(0) a = 'x' = 0 // is there a way to access the variable name of a reference id?
+
+
 
   await waitFor(() => {
     getByText('count: 1')
